585d74d949912805397bc8342d757166f5a7f2a5
CristianCastroRojas <cristiancastro2406@gmail.com>
2023-10-16 18:17:02 -0500

    añadido datos

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menu-btn-toggle');
+const menuList = document.querySelector('ul');
+
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('toggle-style');
+})
